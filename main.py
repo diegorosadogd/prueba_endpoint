1f8e56ef070053a1df9e4cbde9f4e72d507bd4e3
diego_gain <diego.rosado@gaindynamics.com>
2026-03-02 16:54:13 -0600

    actualizando para internet

--- main.py
+++ main.py
@@ -48,5 +48,8 @@ async def get_random_points(count: int = 10):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    import os 
+    host ="0.0.0.0"
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host=host, port=port)
 
